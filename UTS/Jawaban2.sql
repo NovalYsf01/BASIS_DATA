14622c13d3bccf03b33734a967693f0e45996b5a
Noval Yusuf A <145321218+NovalYsf01@users.noreply.github.com>
2023-11-16 21:20:41 +0700

    Create Jawaban2.sql

---
+++ UTS/Jawaban2.sql
@@ -0,0 +1,17 @@
+insert into mata_kuliah
+values 
+( 1, "teknik informatika"),
+( 2, "sistem informasi"),
+( 3, "data analyst");
+
+insert into mahasiswa 
+values
+( 1, "202209211", "taka", "nome", "0859788945", 1 ),
+( 2, "202389273", "beni", "maru", "0856987654", 2 ), 
+( 3, "202105325", "shin", "ra"  , "0854820930", 3 );
+
+insert into dosen 
+values
+( 1, "1142", "yunus", "kabe", 1 ),
+( 2, "1128", "denis", "adit", 2), 
+( 3, "1188", "malik", "arhan", 3);
